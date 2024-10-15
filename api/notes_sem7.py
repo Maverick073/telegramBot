a10481c0090c6a5db5c9be2232f0c9192a6c270f
Sudharshanan <s.sudhar2003@gmail.com>
2024-10-15 08:08:57 +0530

    requirement.txt

--- api/notes_sem7.py
+++ api/notes_sem7.py
@@ -1,6 +1,7 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 from acad_mat import pds_lab 
+from acad_mat import syllabus 
 
 # Handler to send a PDF file
 async def pds_lab(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -13,4 +14,33 @@ async def pds_lab(update: Update, context: ContextTypes.DEFAULT_TYPE):
         document="acad_mat/pds_lab/pds_cia2_algo.pdf"
     )
     
+async def bda_syllabus(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Notify the user
+    await update.message.reply_text("Your requested file is available ....")
+    
+    # Send the PDF file
+    await context.bot.send_document(
+        chat_id=update.effective_chat.id, 
+        document="acad_mat/syllabus/bda_syllabus.pdf"
+    )
+    
+async def iot_syllabus(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Notify the user
+    await update.message.reply_text("Your requested file is available ....")
+    
+    # Send the PDF file
+    await context.bot.send_document(
+        chat_id=update.effective_chat.id, 
+        document="acad_mat/syllabus/iot_syllabus.pdf"
+    )
 
+async def fswd_syllabus(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Notify the user
+    await update.message.reply_text("Your requested file is available ....")
+    
+    # Send the PDF file
+    await context.bot.send_document(
+        chat_id=update.effective_chat.id, 
+        document="acad_mat/syllabus/fswd_syllabus.jpeg"
+    )
+    
